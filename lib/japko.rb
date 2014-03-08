7173fbd9fcd6d236e0c398bff9f2c80e352bdda9
Paweł Pacana <pawel.pacana@gmail.com>
2014-03-08 01:11:42 +0100

    Simplest thing that could work.
    That parslet parser can already parse uncommented .strings like that
    found in In-App Purchase receipts.

--- lib/japko.rb
+++ lib/japko.rb
@@ -1,5 +1,46 @@
-require "japko/version"
+require 'japko/version'
+require 'parslet'
+require 'parslet/convenience'
 
 module Japko
-  # Your code goes here...
+  class Parser < Parslet::Parser
+
+    def quoted(atom)
+      quote >> atom >> quote
+    end
+
+    def curly_braced(atom)
+      str('{') >> atom >> str('}')
+    end
+
+    def maybe_spaced(atom)
+      space? >> atom >> space?
+    end
+
+    rule(:quote)       { str('"') }
+    rule(:equals)      { str('=') }
+    rule(:semicolon)   { str(';') }
+    rule(:linebreak)   { match('\\n') }
+    rule(:linebreak?)  { linebreak.maybe }
+    rule(:space)       { match('\s').repeat(1) }
+    rule(:space?)      { space.maybe }
+    rule(:string)      { quoted((quote.absent? >> any).repeat.as(:string)) }
+    rule(:pair)        { maybe_spaced(string.as(:key)) >> equals >> maybe_spaced(string.as(:value)) >> semicolon }
+    rule(:data)        { curly_braced(linebreak? >> (pair >> linebreak?).repeat) }
+    root(:data)
+
+  end
+
+  class Transform < Parslet::Transform
+
+    rule(:string => simple(:x)) { x.to_s }
+    rule(:key => simple(:key), :value => simple(:value)) { { key => value } }
+
+  end
+
+  def self.parse(data)
+    list_of_pairs = Transform.new.apply(Parser.new.parse(data))
+    list_of_pairs.inject({}) { |hash, pair| hash.merge(pair) }
+  end
+
 end
